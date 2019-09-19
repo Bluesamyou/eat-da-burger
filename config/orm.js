c37cc94cd7bcb42fdbf0ef16863c8f739c43fb86
Shevi <shevids@gmail.com>
2019-09-19 18:39:31 +1000

    Finalised Update logix

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,37 @@
 const connection = require('./connections')
 
+
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+}
+
 const orm = {
-    create: function (table, cols, values),
+    add: function (table, cols, values) {
+
+        return new Promise(function (resolve, reject) {
+            var queryString = "INSERT INTO " + table;
+
+            queryString += " (";
+            queryString += cols.toString();
+            queryString += ") ";
+            queryString += "VALUES (";
+            queryString += printQuestionMarks(values.length);
+            queryString += ") ";
+            console.log()
+            connection.query(queryString, values,
+                function (err, data) {
+                    if (err) throw err
+
+                    resolve(data)
+
+                })
+
+        })
+    },
     read_all: function (table) {
         return new Promise(function (resolve, reject) {
             connection.query('SELECT * FROM ??', [table],
@@ -12,7 +42,7 @@ const orm = {
                 })
         })
     },
-    update: function (table, setCols, matches) {
+    create: function (table, setCols, matches) {
         return new Promise(function (resolve, reject) {
             connection.query(`UPDATE ?? SET ?? WHERE ?? = ?`)
         })
