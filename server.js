28a1ab31e0e81bc8b1b8aa367f2007d766d98236
Shevi <shevids@gmail.com>
2019-09-21 19:04:08 +1000

    Fixed port issue

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const routes = require("./controllers/burgers_controllers");
-const path = require('path')
 
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname + '/public/assets'))
 
